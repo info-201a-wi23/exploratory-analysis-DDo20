a69c2cda90424c28801c427c6a009f1d81ab6449
Abby <amapil@uw.edu>
2023-02-20 14:42:21 -0800

    fix graph

--- preferredsocialmediavssocialmedia.R
+++ preferredsocialmediavssocialmedia.R
@@ -2,6 +2,8 @@ data <- read.csv("https://raw.githubusercontent.com/info-201a-wi23/exploratory-a
 library(dplyr)
 library(ggplot2)
 library(stringr)
+data$Prefered.social.media.platform <- gsub ("Whatsapp", "WhatsApp", data$Prefered.social.media.platform)
+data$Prefered.social.media.platform <- gsub ("None ", "None", data$Prefered.social.media.platform)
 graph_data <- data %>% group_by(Prefered.social.media.platform) %>%
   summarize(avg_time_spent = mean(Time.spent.on.social.media, na.rm = TRUE))
 ggplot(data = graph_data) +
@@ -11,5 +13,6 @@ ggplot(data = graph_data) +
     x = "Platform",
     y = "Average Amount of Hours Spent",
   ) +
-  theme(legend.position="none")
+  theme(legend.position="none") +
+  scale_fill_manual(values = c("#bc66fa", "#1f5aa3", "#fc30a7", "#096be3", "#000000", "#fc8a17", "#a60c0c", "#fa4402", "#ffdd03", "#945adb", "#52d9f7", "#52bbf7", "#04b804", "#fa0202"))
 
